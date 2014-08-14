b6a70eb092c0a24f88411a10e90f0e2a55cc2b83
Brandan Haertel <foilpudding@gmail.com>
2014-08-14 09:03:39 -0700

    made unity testing project

---
+++ New Unity Project/Assets/Camera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	string[] map;
+	// Use this for initialization
+	void Start () {
+		map = System.IO.File.ReadAllLines(@".\map.txt");
+		foreach (string s in map){
+			print(s);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
