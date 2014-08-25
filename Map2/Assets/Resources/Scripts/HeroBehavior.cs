a72d726fe2a2dac8e3389153a0f78614e2ff8aaa
Brandan Haertel <foilpudding@gmail.com>
2014-08-25 11:36:46 -0700

    camera bounds

---
+++ Map2/Assets/Resources/Scripts/HeroBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float H = Input.GetAxis ("Horizontal");
+		float V = Input.GetAxis ("Vertical");
+		this.transform.Translate(new Vector3(H*.05f,V*.05f,0));
+	}
+}
