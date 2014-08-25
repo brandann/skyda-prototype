4f234b42ad8ff9ba3f8d8c2fd55cf70128169b33
Brandan Haertel <foilpudding@gmail.com>
2014-08-25 12:05:12 -0700

    added textures and positioned camera

--- Map2/Assets/Resources/Scripts/GameManager.cs
+++ Map2/Assets/Resources/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	
 	private char[,] map;
+	private int xpos = 1;
+	private int ypos = 1;
+	
+	private int SCREEN_HEIGHT = 9;
+	private int SCREEN_WIDTH = 16;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,5 +19,15 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	
+	public void MoveMap(int dx, int dy){
+		ypos += dy;
+		xpos += dx;
+		LoadMapScreen(xpos, ypos);
+	}
+	
+	private void LoadMapScreen(int x, int y){
+	
 	}
 }
